5235466d34b5a1d7584cbf9bf8477b2d40eb3bfe
Mohamed Badran <41090487+B-a-d-r-a-n@users.noreply.github.com>
2025-06-17 13:17:53 +0300

    Update useAuth.ts

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,9 +1,9 @@
-import { useContext } from "react";
-import { AuthContext } from "../contexts/auth/AuthContext";
-export const useAuth = () => {
-  const context = useContext(AuthContext);
-  if (context === undefined) {
-    throw new Error("useAuth must be used within an AuthProvider");
-  }
-  return context;
-};
+// import { useContext } from "react";
+// import { AuthContext } from "../contexts/auth/AuthContext";
+// export const useAuth = () => {
+//   const context = useContext(AuthContext);
+//   if (context === undefined) {
+//     throw new Error("useAuth must be used within an AuthProvider");
+//   }
+//   return context;
+// };
