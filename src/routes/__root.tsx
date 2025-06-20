7cbcff15264ea330517dd6a366a91fca9a4fbc7b
Mohamed Badran <mohamed.ahmed.badran0@gmail.com>
2025-06-20 23:59:52 +0300

    Toast styling

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,13 +2,14 @@ import NavBar from "../components/ui/NavBar";
 import Footer from "../components/layout/Footer";
 import { FloatingCreateButton } from "../components/ui/FloatingCreateButton";
 import { ScrollToTopButton } from "../components/ui/ScrollToTopButton";
-import { Toaster } from "react-hot-toast";
+// import { Toaster } from "react-hot-toast";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import type { QueryClient } from "@tanstack/react-query";
+import { CustomToaster } from "../components/ui/CustomToaster";
 function RootComponent() {
   return (
     <div className="flex flex-col min-h-screen">
-      <Toaster position="top-center" />
+      <CustomToaster />
       <NavBar />
       <main className="p-2 w-[80%] mx-auto flex-grow">
         <Outlet />
